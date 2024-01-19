a2d433d3f42cd90417fedd585a220d469e91d044
Dayanand Gawade <95270686+Daya3611@users.noreply.github.com>
2024-01-19 23:10:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = 'f547c03d-6f33-4891-a89c-5f7dc409d465';
+import React from 'react';
+
+
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
@@ -12,11 +14,13 @@ const App = () => {
   return (
     <ChatEngine
       height="100vh"
-      projectID="f547c03d-6f33-4891-a89c-5f7dc409d465"
+      projectID="fa24b1ce-5db1-4bdb-98ba-b49cb82f0032"
       userName={localStorage.getItem('username')}
       userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
       onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      renderMessageBubble={(creds, chat, lastMessage, message, nextMessage) => {}}
+      renderIsTyping={(typers) => {}}
     />
   );
 };
